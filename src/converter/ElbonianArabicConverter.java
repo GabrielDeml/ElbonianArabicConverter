eb4759c4436c3a91478f0f376553d7340ba250f7
Mary <mfmarquette@wpi.edu>
2020-04-22 18:21:15 -0400

    working on homework

--- src/converter/ElbonianArabicConverter.java
+++ src/converter/ElbonianArabicConverter.java
@@ -3,6 +3,8 @@ package converter;
 import converter.exceptions.MalformedNumberException;
 import converter.exceptions.ValueOutOfBoundsException;
 
+import java.util.*;
+
 /**
  * This class implements a converter that takes a string that represents a number in either the
  * Elbonian or Arabic numeral form. This class has methods that will return a value in the chosen form.
@@ -12,7 +14,8 @@ import converter.exceptions.ValueOutOfBoundsException;
 public class ElbonianArabicConverter {
 
     // A string that holds the number (Elbonian or Arabic) you would like to convert
-    private final String number;
+    // TODO make final again
+    private String number;
 
     /**
      * Constructor for the ElbonianArabic class that takes a string. The string should contain a valid
@@ -46,15 +49,62 @@ public class ElbonianArabicConverter {
 
 
     public ElbonianArabicConverter(String number) throws MalformedNumberException, ValueOutOfBoundsException {
-      HashMap<String, Char> hashOfLetters = new HashMap<String, Char>;
-      
-      for(int i = 0; i < number.length(), i++){
-        hashOfLetters.put(number.charat(i))
+        this.number = number;
+        number = number.replaceAll("\\s", "");
+        HashMap<Character, Integer> hashOfLetters = new HashMap<Character, Integer>();
+        List<Character> twoBadLetters = Arrays.asList('D', 'E', 'Y', 'Z', 'J', 'K');
+        List<Character> threeBadLetters = Arrays.asList('M', 'C', 'X', 'I');
+        HashSet<Character> hashOf2BadLetters = new HashSet<Character>();
+        HashSet<Character> hashOf3BadLetters = new HashSet<Character>();
+
+        twoBadLetters.stream().map(character -> hashOf2BadLetters.add(character));
+        threeBadLetters.stream().map(character -> hashOf3BadLetters.add(character));
+
+        List<Character> Letters = Arrays.asList('I', 'J', 'K', 'X', 'Y', 'Z', 'C', 'D', 'E', 'M');
+        HashMap<Character, Integer> letterNumConversion = new HashMap<Character, Integer>();
+        for (int i = 0; i < Letters.size(); i++) {
+            letterNumConversion.put(Letters.get(i), i);
+        }
+
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) < )
+        }
+
+      for(int i = 0; i < number.length(); i++){
+          if (Character.isLowerCase(number.charAt(i))) {
+              throw new MalformedNumberException("error lowercase");
+          }
+          if (hashOfLetters.containsKey(number.charAt(i))) {
+            hashOfLetters.put(number.charAt(i), hashOfLetters.get(i) + 1);
+        } else {
+            hashOfLetters.put(number.charAt(i), 1);
+        }
+
       }
+      for (int i = 0; i < number.length(); i++) {
+          if (hashOf2BadLetters.contains(number.charAt(i))) {
+              if (hashOfLetters.get(number.charAt(i)) >= 2) {
+                  throw new MalformedNumberException("error 2 bad letters");
+              }
+          }
+          if (hashOf3BadLetters.contains(number.charAt(i))) {
+              if (hashOfLetters.get(number.charAt(i)) >= 3) {
+                  throw new MalformedNumberException("error 3 bad letters");
+              }
+          }
+      }
+      if (hashOfLetters.containsKey('D') & hashOfLetters.containsKey('E') & hashOfLetters.containsKey('C')) {
+          throw new MalformedNumberException("error D, E, C appeared");
+      }
+        if (hashOfLetters.containsKey('Y') & hashOfLetters.containsKey('Z') & hashOfLetters.containsKey('X')) {
+            throw new MalformedNumberException("error Y, Z, X appeared");
+        }
+        if (hashOfLetters.containsKey('J') & hashOfLetters.containsKey('K') & hashOfLetters.containsKey('I')) {
+            throw new MalformedNumberException("error J, K, I appeared");
+        }
 
         // TODO check to see if the number is valid, then set it equal to the string
-        number = number.replaceAll("\\s", "");
-        this.number = number;
+
     }
 
     /**
